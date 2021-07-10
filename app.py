92265c97b5c782b790efb8e1466ae3282155ab39
Roshni Patil <68719250+Roshni-Patil@users.noreply.github.com>
2021-07-10 23:54:00 +0530

    data retrieve

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def home():
 
 @app.route("/pred" , methods = ["GET" ] )
 def prediction():
+        name= request.args.get('name')
         x1 = request.args.get( "z1" )
         x2 = request.args.get( "z2" )
         x3 = request.args.get( "z3" )
@@ -34,10 +35,10 @@ def prediction():
         output = model.predict([[ int(x1)  , int(x2) , int( x3) , int(x4) , int(x5) , float(x6) , float(x7) , int(x8)]])
         ans =  str(round(output[0][0] ))
         if int(ans) == 0:
-                return render_template( "diano.html")
+                return render_template( "diano.html", name=name ,x1=x1 , x2=x2 , x3=x3, x4=x4 , x5=x5, x6=x6 , x7=x7 , x8=x8)
 
         else :
-                return render_template( "diayes.html")
+                return render_template( "diayes.html",  name=name ,x1=x1 , x2=x2 , x3=x3, x4=x4 , x5=x5, x6=x6 , x7=x7 , x8=x8)
 
 
 app.run(host="172.17.0.2" , port=8080, debug=True)
